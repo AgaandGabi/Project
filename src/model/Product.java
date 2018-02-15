e19f1c3e936c059cad0f9cb72687ae05a490823d
GabiAntohi <gabrielaantohi.eu@gmail.com>
2018-02-15 09:00:40 +0000

    "15.02.2018 - commit"

--- src/model/Product.java
+++ src/model/Product.java
@@ -15,32 +15,29 @@ public class Product {
 	@Column
 	private String name;
 	@Column
-	private String description;
-	@Column
 	private BigDecimal price;
 	@Column
-	private BigDecimal weight;
-	@Column
 	private int quantity;
+	@Column
+	private int weight;
 	
 	public Product() {}
 
-	public Product(int id, String name, String description, BigDecimal price, BigDecimal weight, int quantity) {
+	public Product(int id, String name, BigDecimal price, int quantity, int weight) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.description = description;
 		this.price = price;
-		this.weight = weight;
 		this.quantity = quantity;
+		this.weight = weight;
 	}
-	public Product( String name, String description, BigDecimal price, BigDecimal weight, int quantity) {
-
+	public Product(String name, BigDecimal price, int quantity, int weight) {
+		super();
+		
 		this.name = name;
-		this.description = description;
 		this.price = price;
-		this.weight = weight;
 		this.quantity = quantity;
+		this.weight = weight;
 	}
 
 	public int getId() {
@@ -59,28 +56,12 @@ public class Product {
 		this.name = name;
 	}
 
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 	public BigDecimal getPrice() {
 		return price;
 	}
 
 	public void setPrice(BigDecimal price) {
 		this.price = price;
-	}
-
-	public BigDecimal getWeight() {
-		return weight;
-	}
-
-	public void setWeight(BigDecimal weight) {
-		this.weight = weight;
 	}
 
 	public int getQuantity() {
@@ -91,16 +72,23 @@ public class Product {
 		this.quantity = quantity;
 	}
 
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
 		result = prime * result + id;
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((price == null) ? 0 : price.hashCode());
 		result = prime * result + quantity;
-		result = prime * result + ((weight == null) ? 0 : weight.hashCode());
+		result = prime * result + weight;
 		return result;
 	}
 
@@ -113,11 +101,6 @@ public class Product {
 		if (getClass() != obj.getClass())
 			return false;
 		Product other = (Product) obj;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
 		if (id != other.id)
 			return false;
 		if (name == null) {
@@ -132,18 +115,15 @@ public class Product {
 			return false;
 		if (quantity != other.quantity)
 			return false;
-		if (weight == null) {
-			if (other.weight != null)
-				return false;
-		} else if (!weight.equals(other.weight))
+		if (weight != other.weight)
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
-				+ ", weight=" + weight + ", quantity=" + quantity + "]";
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity + ", weight="
+				+ weight + "]";
 	}
-
+	
 }
